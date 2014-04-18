dccaf83b86eee87901acf92f0534bac4b411277f
Pengfei Zhang <zpfalpc23@gmail.com>
2014-04-18 23:34:31 +0800

    Add more solutions to the easy level problems.
    The new added problems' number is: 61, 62, 81, 97, 122, 134, 147, 157.

--- answers/easy.clj
+++ answers/easy.clj
@@ -45,3 +45,48 @@ filter odd?
 ;; Write a function which returns true if the given sequence is a palindrome.
 #(= (reverse %) (seq %))
 
+;; Problem 61. Map Construction
+;; Write a function which takes a vector of keys and a vector of values and constructs a map from them.
+;; chouser's solution
+#(into {} (map vector % %2))
+
+;; Problem 62. Re-implement Iterate
+;; Given a side-effect free function f and an initial value x write a function which returns an infinite lazy sequence of x, (f x), (f
+;; (f x)), (f (f (f x))), etc.
+;; dacquiri's solution
+#(tree-seq % (juxt %) %2)
+
+;; Problem 81. Set Intersection
+;; Write a function which returns the intersection of two sets. The intersection is the sub-set of items that each set has in common.
+;; aceeca1's solution
+(comp set filter)
+
+;; Problem 97. Pascal's Triangle
+;; Write a function which returns the nth row of Pascal's Triangle. 
+;; baex's solution
+#(nth (iterate (fn [x] (map + `(0 ~@x) `(~@x 0))) [1]) (- % 1))
+
+;; Problem 122. Read a binary number
+;; Convert a binary number, provided in the form of a string, to its numerical value.
+;; adereth's solution
+#(Integer/parseInt % 2)
+
+;; Problem 134. Infix Calculator
+;; Your friend Joe is always whining about Lisps using the prefix notation for math. Show him how you could easily write a function
+;; that does math using the infix notation. Is your favorite language that flexible, Joe? Write a function that accepts a variable
+;; length mathematical expression consisting of numbers and the operations +, -, *, and /. Assume a simple calculator that does not do
+;; precedence and instead just calculates left to right.
+;; sheldon's solution
+(fn i ([r] r) ([l o r & m] (apply i (o l r) m)))
+
+;; Problem 147. Pascal's Trapezoid
+;; Write a function that, for any given input vector of numbers, returns an infinite lazy sequence of vectors, where each next one is
+;; constructed from the previous following the rules used in Pascal's Triangle. For example, for [3 1 2], the next row is [3 4 3 2].
+;; awebb's solution
+iterate #(map +' `(0 ~@%) `(~@% 0))
+
+;; Problem 157. Indexing Sequences
+;; Transform a sequence into a sequence of pairs containing the original elements along with their index.
+;; austintaylor's solution
+#(map list % (range))
+
